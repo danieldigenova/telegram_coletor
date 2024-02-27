d4df2cc4f0f9a33a17191d5584f10ad4e8d28080
danieldigenova <58534252+danieldigenova@users.noreply.github.com>
2024-02-27 19:07:43 -0300

    Update telegram_coletor.py

--- telegram_coletor.py
+++ telegram_coletor.py
@@ -1,15 +1,15 @@
-from telethon.sync import TelegramClient  # Permite a comunicação síncrona com a API do Telegram.
-import asyncio                           # Biblioteca para escrever código assíncrono em Python.
-import nest_asyncio                      # Torna possível usar loops asyncio aninhados.
-import datetime                          # Fornece classes para manipulação de datas e horas.
-import time                              # Contém funções de tempo, como delay (sleep).
-import json                              # Facilita a codificação e decodificação de dados em formato JSON.
-import pytz                              # Oferece suporte para fusos horários.
-import re                                # Fornece operações com expressões regulares.
-import pandas as pd                      # Biblioteca para manipulação e análise de dados
+from telethon.sync import TelegramClient                          # Permite a comunicação síncrona com a API do Telegram.
+from telethon.tl.functions.channels import GetFullChannelRequest  # Permite a coleta de informações do canal
+import asyncio                                                    # Biblioteca para escrever código assíncrono em Python.
+import nest_asyncio                                               # Torna possível usar loops asyncio aninhados.
+import datetime                                                   # Fornece classes para manipulação de datas e horas.
+import time                                                       # Contém funções de tempo, como delay (sleep).
+import json                                                       # Facilita a codificação e decodificação de dados em formato JSON.
+import pytz                                                       # Oferece suporte para fusos horários.
+import re                                                         # Fornece operações com expressões regulares.
+import pandas as pd                                               # Biblioteca para manipulação e análise de dados.
 
 nest_asyncio.apply() # Aplica um patch para permitir que loops asyncio aninhados funcionem corretamente.
-
 
 # função que coleta as mensagens e salva em um arquivo json
 async def coletar(name, api_id, api_hash, filename, termos, canais, data_inicio, data_fim):
@@ -20,7 +20,7 @@ async def coletar(name, api_id, api_hash, filename, termos, canais, data_inicio,
     with open(filename + '.json', 'a+') as json_file:
         async with TelegramClient(name, api_id, api_hash) as client:
             for canal in canais:
-                messages = await client.get_messages(canal, limit=5000, offset_date = data_f)
+                messages = await client.get_messages(canal, limit=4000, offset_date = data_f)
                 for message in messages:
 
                     # encerra se a mensagem for mais antiga que a data inicial estabelecida
@@ -37,6 +37,20 @@ async def coletar(name, api_id, api_hash, filename, termos, canais, data_inicio,
 
                         # recebe as informações do canal
                         channel = await client.get_entity(message.peer_id)
+
+                        channel_full_info = await client(GetFullChannelRequest(channel=channel))
+                        participants_count = channel_full_info.full_chat.participants_count
+
+                        group_type = '';
+
+                        if(channel_full_info.chats[0].megagroup):
+                            group_type = 'megagroup'
+                        elif(channel_full_info.chats[0].broadcast):
+                            group_type = 'broadcast'
+                        elif(channel_full_info.chats[0].gigagroup):
+                            group_type = 'gigagroup'
+
+                        #group_type
 
                         # verifica qual é o tipo de usuário
                         if(message.from_id != None):
@@ -82,21 +96,44 @@ async def coletar(name, api_id, api_hash, filename, termos, canais, data_inicio,
                         else:
                             reply_to = None
 
+
+                        # verifica se a mensagem é um encaminhamento
+                        fwd_from = ''
+                        fwd_from_id = ''
+                        fwd_from_name = ''
+                        fwd_from_post_id = ''
+
+                        if(message.fwd_from != None):
+                          if(hasattr(message.fwd_from.from_id, "channel_id")):
+                            fwd_from_id = message.fwd_from.from_id.channel_id
+                            fwd_from_post_id = message.fwd_from.channel_post
+                            fwd_from = 'channel'
+                            fwd_from_name = message.fwd_from.from_name
+                          elif(hasattr(message.fwd_from.from_id, "user_id")):
+                            fwd_from_id = message.fwd_from.from_id.user_id
+                            fwd_from_post_id = message.fwd_from.channel_post
+                            fwd_from = 'user'
+                            fwd_from_name = message.fwd_from.from_name
+                          else:
+                            fwd_from_id = message.fwd_from.from_id
+                            fwd_from_post_id = message.fwd_from.channel_post
+                            fwd_from_name = message.fwd_from.from_name
+
                         # constrói o json e salva em um arquivo
-                        try:
-                            json_tweet = {"id":message.id, "date":str(message.date), "text":message.message,
-                                          "mentioned":message.mentioned, "post_author":message.post_author,
-                                          "reply_to":reply_to, "views":message.views, "replies":comments,
-                                          "forwards":message.forwards, "out":message.out,
-                                          "channel_title":channel.title, "channel_username":channel.username,
-                                          "channel_id": message.peer_id.channel_id,
+                        #try:
+                        json_tweet = {    "id":message.id, "date":str(message.date), "text":message.message,
+                                          "post_author":message.post_author, "reply_to_message_id":reply_to,
+                                          "views":message.views, "replies":comments, "forwards":message.forwards,
+                                          "fwd_from":fwd_from, "fwd_from_name":fwd_from_name,
+                                          "forward_from_id":fwd_from_id, "forward_from_post_id":fwd_from_post_id,
+                                          "channel_title":channel.title, "channel_username":channel.username, "group_type":group_type,
+                                          "channel_id": message.peer_id.channel_id, "participants_count": participants_count,
                                           "user_id":user_id, "user_username":user_username, "user_first_name":first_name,
                                           "user_last_name":last_name, "channel_username_chat":channel_username}
 
-                            json.dump(json_tweet, json_file)
-                            json_file.write('\n')
-                        except:
-                            continue
+                        json.dump(json_tweet, json_file)
+                        json_file.write('\n')
+                        #except:
+                            #continue
                 print("Mensagens do canal " + canal + " coletadas.")
                 json_file.flush()
-                time.sleep(60)
