c0eaa26c4cdc3e71769ceec21e01ecfc7a85ad20
danieldigenova <58534252+danieldigenova@users.noreply.github.com>
2024-02-28 10:34:48 -0300

    Update telegram_coletor.py

--- telegram_coletor.py
+++ telegram_coletor.py
@@ -1,5 +1,6 @@
 from telethon.sync import TelegramClient                          # Permite a comunicação síncrona com a API do Telegram.
 from telethon.tl.functions.channels import GetFullChannelRequest  # Permite a coleta de informações do canal
+from urlextract import URLExtract                                 # Biblioteca para extração de URLs
 import asyncio                                                    # Biblioteca para escrever código assíncrono em Python.
 import nest_asyncio                                               # Torna possível usar loops asyncio aninhados.
 import datetime                                                   # Fornece classes para manipulação de datas e horas.
@@ -20,7 +21,7 @@ async def coletar(name, api_id, api_hash, filename, termos, canais, data_inicio,
     with open(filename + '.json', 'a+') as json_file:
         async with TelegramClient(name, api_id, api_hash) as client:
             for canal in canais:
-                messages = await client.get_messages(canal, limit=4000, offset_date = data_f)
+                messages = await client.get_messages(canal, limit=1000, offset_date = data_f)
                 for message in messages:
 
                     # encerra se a mensagem for mais antiga que a data inicial estabelecida
@@ -90,6 +91,15 @@ async def coletar(name, api_id, api_hash, filename, termos, canais, data_inicio,
                         except:
                             comments = 0
 
+                        if(comments == 0):
+                            if(message.replies == None):
+                              replies = 0
+                            else:
+                              replies = message.replies.replies
+                        
+                        else:
+                            replies = comments
+
                         # verifica se a mensagem é uma resposta
                         if(message.reply_to != None):
                             reply_to = message.reply_to.reply_to_msg_id
@@ -118,12 +128,20 @@ async def coletar(name, api_id, api_hash, filename, termos, canais, data_inicio,
                             fwd_from_id = message.fwd_from.from_id
                             fwd_from_post_id = message.fwd_from.channel_post
                             fwd_from_name = message.fwd_from.from_name
+                        
+                        # extrai links presentes na mensagem
+                        extractor = URLExtract()
+                        links = extractor.find_urls(message.message)
+
+                        # extrai menções presentes na mensagem
+                        mentions = re.findall("(?<!\S)@(\w+)", message.message)
 
                         # constrói o json e salva em um arquivo
                         #try:
                         json_tweet = {    "id":message.id, "date":str(message.date), "text":message.message,
                                           "post_author":message.post_author, "reply_to_message_id":reply_to,
-                                          "views":message.views, "replies":comments, "forwards":message.forwards,
+                                          "views":message.views, "replies":replies, "forwards":message.forwards, 
+                                          "mentions":mentions, "links":links,
                                           "fwd_from":fwd_from, "fwd_from_name":fwd_from_name,
                                           "forward_from_id":fwd_from_id, "forward_from_post_id":fwd_from_post_id,
                                           "channel_title":channel.title, "channel_username":channel.username, "group_type":group_type,
