a636da97d5c831705273cc6e8ffe7f9d3e913008
danieldigenova <58534252+danieldigenova@users.noreply.github.com>
2024-04-03 11:02:39 -0300

    coleta todas mensagens e constrói link

--- telegram_coletor.py
+++ telegram_coletor.py
@@ -65,136 +65,126 @@ async def coletar(name, api_id, api_hash, filename, termos, canais, limite, data
                       if(message.date < data_i):
                         break
 
-                      # se a lista de termos for vazia então coleta todas mensagens
-                      if(termos == []):
-                        check = True
-                      # se a lista de termos ter algum termo, então realiza a verificação dos termos
-                      else:
-                        texto = str(message.message).lower()
-                        palavras = texto.split()
+                      # recebe as informações do canal
+                      channel = await client.get_entity(message.peer_id)
 
-                        # verifica se a mensagem possui algum dos termos presentes na lista fornecida
-                        check = any(termo in termos for termo in palavras)
+                      channel_full_info = await client(GetFullChannelRequest(channel=channel))
+                      participants_count = channel_full_info.full_chat.participants_count
 
-                      if(check):
+                      group_type = '';
 
-                          # recebe as informações do canal
-                          channel = await client.get_entity(message.peer_id)
+                      if(channel_full_info.chats[0].megagroup):
+                          group_type = 'megagroup'
+                      elif(channel_full_info.chats[0].broadcast):
+                          group_type = 'broadcast'
+                      elif(channel_full_info.chats[0].gigagroup):
+                          group_type = 'gigagroup'
 
-                          channel_full_info = await client(GetFullChannelRequest(channel=channel))
-                          participants_count = channel_full_info.full_chat.participants_count
+                      # verifica qual é o tipo de usuário
+                      if(message.from_id != None):
+                          try:
+                              user = await client.get_entity(message.from_id)
+                              user_id = user.id
 
-                          group_type = '';
-
-                          if(channel_full_info.chats[0].megagroup):
-                              group_type = 'megagroup'
-                          elif(channel_full_info.chats[0].broadcast):
-                              group_type = 'broadcast'
-                          elif(channel_full_info.chats[0].gigagroup):
-                              group_type = 'gigagroup'
-
-                          # verifica qual é o tipo de usuário
-                          if(message.from_id != None):
-                              try:
-                                  user = await client.get_entity(message.from_id)
-                                  user_id = user.id
-
-                                  if(hasattr(user, "first_name")):
-                                      channel_username = None
-                                      user_username = user.username
-                                      first_name = user.first_name
-                                      last_name = user.last_name
-                                  else:
-                                      channel_username = user.username
-                                      user_username = None
-                                      first_name = None
-                                      last_name = None
-                              except:
-                                  user_id = message.from_id
+                              if(hasattr(user, "first_name")):
                                   channel_username = None
+                                  user_username = user.username
+                                  first_name = user.first_name
+                                  last_name = user.last_name
+                              else:
+                                  channel_username = user.username
                                   user_username = None
                                   first_name = None
                                   last_name = None
-                          else:
-                              user_id = None
+                          except:
+                              user_id = message.from_id
                               channel_username = None
                               user_username = None
                               first_name = None
                               last_name = None
+                      else:
+                          user_id = None
+                          channel_username = None
+                          user_username = None
+                          first_name = None
+                          last_name = None
 
-                          # extrai o número de comentários exibido no botão do Telegram
-                          try:
-                              comments_text = re.findall("\d+ comments", message.reply_markup.rows[0].buttons[0].text)[0]
+                      # extrai o número de comentários exibido no botão do Telegram
+                      try:
+                          comments_text = re.findall("\d+ comments", message.reply_markup.rows[0].buttons[0].text)[0]
 
-                              if(comments_text != ''):
-                                  comments = re.findall("\d+", message.reply_markup.rows[0].buttons[0].text)[0]
-                          except:
-                              comments = 0
+                          if(comments_text != ''):
+                              comments = re.findall("\d+", message.reply_markup.rows[0].buttons[0].text)[0]
+                      except:
+                          comments = 0
 
-                          if(comments == 0):
-                              if(message.replies == None):
-                                replies = 0
-                              else:
-                                replies = message.replies.replies
-                          
+                      if(comments == 0):
+                          if(message.replies == None):
+                            replies = 0
                           else:
-                              replies = comments
+                            replies = message.replies.replies
+                      
+                      else:
+                          replies = comments
 
-                          # verifica se a mensagem é uma resposta
-                          if(message.reply_to != None):
-                              reply_to = message.reply_to.reply_to_msg_id
-                          else:
-                              reply_to = None
+                      # verifica se a mensagem é uma resposta
+                      if(message.reply_to != None):
+                          reply_to = message.reply_to.reply_to_msg_id
+                      else:
+                          reply_to = None
 
-                          # verifica se a mensagem é um encaminhamento
-                          fwd_from = ''
-                          fwd_from_id = ''
-                          fwd_from_name = ''
-                          fwd_from_post_id = ''
+                      # verifica se a mensagem é um encaminhamento
+                      fwd_from = ''
+                      fwd_from_id = ''
+                      fwd_from_name = ''
+                      fwd_from_post_id = ''
 
-                          if(message.fwd_from != None):
-                            if(hasattr(message.fwd_from.from_id, "channel_id")):
-                              fwd_from_id = message.fwd_from.from_id.channel_id
-                              fwd_from_post_id = message.fwd_from.channel_post
-                              fwd_from = 'channel'
-                              fwd_from_name = message.fwd_from.from_name
-                            elif(hasattr(message.fwd_from.from_id, "user_id")):
-                              fwd_from_id = message.fwd_from.from_id.user_id
-                              fwd_from_post_id = message.fwd_from.channel_post
-                              fwd_from = 'user'
-                              fwd_from_name = message.fwd_from.from_name
-                            else:
-                              fwd_from_id = message.fwd_from.from_id
-                              fwd_from_post_id = message.fwd_from.channel_post
-                              fwd_from_name = message.fwd_from.from_name
-                          
-                          # extrai links presentes na mensagem
-                          extractor = URLExtract()
-                          try:
-                            links = extractor.find_urls(message.message)
-                          except:
-                            links = []
+                      if(message.fwd_from != None):
+                        if(hasattr(message.fwd_from.from_id, "channel_id")):
+                          fwd_from_id = message.fwd_from.from_id.channel_id
+                          fwd_from_post_id = message.fwd_from.channel_post
+                          fwd_from = 'channel'
+                          fwd_from_name = message.fwd_from.from_name
+                        elif(hasattr(message.fwd_from.from_id, "user_id")):
+                          fwd_from_id = message.fwd_from.from_id.user_id
+                          fwd_from_post_id = message.fwd_from.channel_post
+                          fwd_from = 'user'
+                          fwd_from_name = message.fwd_from.from_name
+                        else:
+                          fwd_from_id = message.fwd_from.from_id
+                          fwd_from_post_id = message.fwd_from.channel_post
+                          fwd_from_name = message.fwd_from.from_name
+                      
+                      # extrai links presentes na mensagem
+                      extractor = URLExtract()
+                      try:
+                        links = extractor.find_urls(message.message)
+                      except:
+                        links = []
 
-                          # extrai menções presentes na mensagem
-                          try:
-                            mentions = re.findall("(?<!\S)@(\w+)", message.message)
-                          except:
-                            mentions = []
+                      # extrai menções presentes na mensagem
+                      try:
+                        mentions = re.findall("(?<!\S)@(\w+)", message.message)
+                      except:
+                        mentions = []
 
-                          # constrói o json e salva em um arquivo
-                          json_tweet = {    "id":message.id, "date":str(message.date), "text":message.message,
-                                            "post_author":message.post_author, "reply_to_message_id":reply_to,
-                                            "views":message.views, "replies":replies, "forwards":message.forwards, 
-                                            "mentions":mentions, "links":links,
-                                            "fwd_from":fwd_from, "fwd_from_name":fwd_from_name,
-                                            "forward_from_id":fwd_from_id, "forward_from_post_id":fwd_from_post_id,
-                                            "channel_title":channel.title, "channel_username":channel.username, "group_type":group_type,
-                                            "channel_id": message.peer_id.channel_id, "participants_count": participants_count,
-                                            "user_id":user_id, "user_username":user_username, "user_first_name":first_name,
-                                            "user_last_name":last_name, "channel_username_chat":channel_username}
+                      # constroi link direto para a mensagem
+                      msg_link = "https://t.me/" + canal + "/" + str(message.id);
 
-                          json.dump(json_tweet, json_file)
-                          json_file.write('\n')
+                     # constrói o json e salva em um arquivo
+                      json_tweet = {    "id":message.id, "date":str(message.date), "text":message.message,
+                                        "post_author":message.post_author, "reply_to_message_id":reply_to,
+                                        "views":message.views, "replies":replies, "forwards":message.forwards,
+                                        "mentions":mentions, "links":links, "msg_link": msg_link,
+                                        "fwd_from":fwd_from, "fwd_from_name":fwd_from_name,
+                                        "forward_from_id":fwd_from_id, "forward_from_post_id":fwd_from_post_id,
+                                        "channel_title":channel.title, "channel_username":channel.username, "group_type":group_type,
+                                        "channel_id": message.peer_id.channel_id, "participants_count": participants_count,
+                                        "user_id":user_id, "user_username":user_username, "user_first_name":first_name,
+                                        "user_last_name":last_name, "channel_username_chat":channel_username}
+
+                      json.dump(json_tweet, json_file)
+                      json_file.write('\n')
 
                   json_file.flush()
                   print("Canal " + canal + " coletado.")
